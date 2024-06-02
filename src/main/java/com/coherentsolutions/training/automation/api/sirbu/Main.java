8667c830a0b4a541ecc52d809e9b341fef134cfe
VitalieSirbu <vitalie.sirbu0@gmail.com>
2024-06-03 01:44:07 +0300

    IMplementing SneakyThows to Main

--- src/main/java/com/coherentsolutions/training/automation/api/sirbu/Main.java
+++ src/main/java/com/coherentsolutions/training/automation/api/sirbu/Main.java
@@ -1,17 +1,17 @@
 package com.coherentsolutions.training.automation.api.sirbu;
 
+import lombok.SneakyThrows;
+
 public class Main {
+
+    @SneakyThrows
     public static void main(String[] args) {
         AuthProvider authProvider = AuthProvider.getInstance();
-        try {
-            String writeToken = authProvider.getWriteToken();
-            String readToken = authProvider.getReadToken();
 
-            System.out.println("Write Token: " + writeToken);
-            System.out.println("Read Token: " + readToken);
-        } catch (Exception e) {
-            System.err.println("Error occurred while getting tokens: " + e.getMessage());
-            e.printStackTrace();
-        }
+        String writeToken = authProvider.getWriteToken();
+        String readToken = authProvider.getReadToken();
+
+        System.out.println("Write Token: " + writeToken);
+        System.out.println("Read Token: " + readToken);
     }
 }
