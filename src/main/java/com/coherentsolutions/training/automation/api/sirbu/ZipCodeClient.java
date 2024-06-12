897094e4871fc1634eaee1099bc561a063d1b190
VitalieSirbu <vitalie.sirbu0@gmail.com>
2024-06-12 19:17:21 +0300

    Implementing ZipCodeClient class and objectMapper method for deserialize response.

---
+++ src/main/java/com/coherentsolutions/training/automation/api/sirbu/ZipCodeClient.java
@@ -0,0 +1,60 @@
+package com.coherentsolutions.training.automation.api.sirbu;
+
+import com.coherentsolutions.training.automation.api.sirbu.Utils.NoResponseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ZipCodeClient {
+
+    private final CloseableHttpClient client;
+    private final ObjectMapper objectMapper;
+
+    public ZipCodeClient() {
+        this.client = HttpClients.createDefault();
+        this.objectMapper = new ObjectMapper();
+    }
+
+    public List<String> getZipCodes(String url, String token) throws IOException, NoResponseException {
+        HttpGet get = new HttpGet(url);
+        get.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+
+        CloseableHttpResponse response = client.execute(get);
+
+        if (response == null) {
+            throw new NoResponseException("No response received from the server");
+        }
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode != 200) {
+            throw new RuntimeException("Failed to get zip codes. Status code: " + statusCode);
+        }
+
+        String responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
+        return objectMapper.readValue(responseBody, List.class);
+    }
+
+    @SneakyThrows
+    public CloseableHttpResponse postZipCodes(String url, String token, String requestBody) {
+        HttpPost post = new HttpPost(url);
+        post.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        post.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        post.setEntity(new StringEntity(requestBody, "UTF-8"));
+
+        return client.execute(post);
+    }
+
+    public void close() throws IOException {
+        client.close();
+    }
+}
--- src/test/java/com/coherentsolutions/training/automation/api/sirbu/ZipCodeTest.java
+++ src/test/java/com/coherentsolutions/training/automation/api/sirbu/ZipCodeTest.java
@@ -1,22 +1,13 @@
 package com.coherentsolutions.training.automation.api.sirbu;
 
 import com.coherentsolutions.training.automation.api.sirbu.Utils.ConfigLoader;
-import com.coherentsolutions.training.automation.api.sirbu.Utils.NoResponseException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
-import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static com.coherentsolutions.training.automation.api.sirbu.Utils.Constants.NUMBER_OF_ZIP_CODES;
@@ -26,143 +17,101 @@ public class ZipCodeTest {
     @Test
     @SneakyThrows
     public void testGetZipCodes() {
+        ZipCodeClient zipCodeClient = new ZipCodeClient();
 
         AuthProvider authProvider = AuthProvider.getInstance();
         String readToken = authProvider.getReadToken();
 
         String zipCodesUrl = ConfigLoader.getProperty("zipCodesUrl");
 
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpGet get = new HttpGet(zipCodesUrl);
-        get.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + readToken);
+        List<String> zipCodesList = zipCodeClient.getZipCodes(zipCodesUrl, readToken);
+        System.out.println("Current zip codes are: " + zipCodesList);
 
-        CloseableHttpResponse response = client.execute(get);
+        Assert.assertTrue(!zipCodesList.isEmpty());
 
-        if (response == null) {
-            throw new NoResponseException("No response received from the server");
-        }
-
-        int statusCode = response.getStatusLine().getStatusCode();
-        Assert.assertEquals(201, statusCode);
-
-        String responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode zipCodesNode = objectMapper.readTree(responseBody);
-        System.out.println("Current zip codes are: " + zipCodesNode);
-
-        Assert.assertTrue(zipCodesNode.isArray());
-        Assert.assertTrue(!zipCodesNode.isEmpty());
-
-        client.close();
+        zipCodeClient.close();
     }
 
     @Test
     @SneakyThrows
     public void testPostZipCodes() {
+        ZipCodeClient zipCodeClient = new ZipCodeClient();
 
-        try (CloseableHttpClient client = HttpClients.createDefault()) {
+        AuthProvider authProvider = AuthProvider.getInstance();
+        String writeToken = authProvider.getWriteToken();
+        String readToken = authProvider.getReadToken();
 
-            AuthProvider authProvider = AuthProvider.getInstance();
-            String writeToken = authProvider.getWriteToken();
+        String zipCodesExpandUrl = ConfigLoader.getProperty("zipCodesExpandUrl");
+        String requestBody = "[\"12345fhdrhdh414\", \"67890\"]";
 
-            String zipCodesExpandUrl = ConfigLoader.getProperty("zipCodesExpandUrl");
+        CloseableHttpResponse response = zipCodeClient.postZipCodes(zipCodesExpandUrl, writeToken, requestBody);
 
-            String requestBody = "[\"12345\", \"67890\"]";
+        int statusCode = response.getStatusLine().getStatusCode();
+        Assert.assertEquals(201, statusCode);
 
-            HttpPost post = new HttpPost(zipCodesExpandUrl);
-            post.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + writeToken);
-            post.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-            post.setEntity(new StringEntity(requestBody, "UTF-8"));
+        String zipCodesUrl = ConfigLoader.getProperty("zipCodesUrl");
+        List<String> zipCodesList = zipCodeClient.getZipCodes(zipCodesUrl, readToken);
+        Set<String> zipCodesSet = new HashSet<>(zipCodesList);
 
-            CloseableHttpResponse response = client.execute(post);
+        Assert.assertTrue(zipCodesSet.contains("12345"));
+        Assert.assertTrue(zipCodesSet.contains("67890"));
 
-            int statusCode = response.getStatusLine().getStatusCode();
-            Assert.assertEquals(201, statusCode);
-
-            HttpGet get = new HttpGet(ConfigLoader.getProperty("zipCodesUrl"));
-            get.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authProvider.getReadToken());
-            CloseableHttpResponse getResponse = client.execute(get);
-            String getResponseBody = EntityUtils.toString(getResponse.getEntity(), "UTF-8");
-            JsonNode zipCodesNode = new ObjectMapper().readTree(getResponseBody);
-            Set<String> zipCodesSet = new HashSet<>();
-            zipCodesNode.forEach(zipCode -> zipCodesSet.add(zipCode.asText()));
-
-            Assert.assertTrue(zipCodesSet.contains("12345"));
-            Assert.assertTrue(zipCodesSet.contains("67890"));
-        }
+        zipCodeClient.close();
     }
+
     @Test
     @SneakyThrows
     public void testExpandZipCodesWithDuplications() {
+        ZipCodeClient zipCodeClient = new ZipCodeClient();
 
-        try (CloseableHttpClient client = HttpClients.createDefault()) {
+        AuthProvider authProvider = AuthProvider.getInstance();
+        String writeToken = authProvider.getWriteToken();
+        String readToken = authProvider.getReadToken();
 
-            AuthProvider authProvider = AuthProvider.getInstance();
-            String writeToken = authProvider.getWriteToken();
+        String zipCodesExpandUrl = ConfigLoader.getProperty("zipCodesExpandUrl");
+        String requestBody = "[\"12345\", \"67890\", \"12345\"]";
 
-            String zipCodesExpandUrl = ConfigLoader.getProperty("zipCodesExpandUrl");
+        CloseableHttpResponse response = zipCodeClient.postZipCodes(zipCodesExpandUrl, writeToken, requestBody);
 
-            String requestBody = "[\"12345\", \"67890\", \"12345\"]";
+        int statusCode = response.getStatusLine().getStatusCode();
+        Assert.assertEquals(201, statusCode);
 
-            HttpPost post = new HttpPost(zipCodesExpandUrl);
-            post.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + writeToken);
-            post.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-            post.setEntity(new StringEntity(requestBody, "UTF-8"));
+        String zipCodesUrl = ConfigLoader.getProperty("zipCodesUrl");
+        List<String> zipCodesList = zipCodeClient.getZipCodes(zipCodesUrl, readToken);
+        Set<String> zipCodesSet = new HashSet<>(zipCodesList);
 
-            CloseableHttpResponse response = client.execute(post);
+        Assert.assertTrue(zipCodesSet.contains("12345"));
+        Assert.assertTrue(zipCodesSet.contains("67890"));
+        Assert.assertEquals(NUMBER_OF_ZIP_CODES.intValue(), zipCodesSet.size());
 
-            int statusCode = response.getStatusLine().getStatusCode();
-            Assert.assertEquals(201, statusCode);
-
-            HttpGet get = new HttpGet(ConfigLoader.getProperty("zipCodesUrl"));
-            get.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authProvider.getReadToken());
-            CloseableHttpResponse getResponse = client.execute(get);
-            String getResponseBody = EntityUtils.toString(getResponse.getEntity(), "UTF-8");
-            JsonNode zipCodesNode = new ObjectMapper().readTree(getResponseBody);
-            Set<String> zipCodesSet = new HashSet<>();
-            zipCodesNode.forEach(zipCode -> zipCodesSet.add(zipCode.asText()));
-
-            Assert.assertTrue(zipCodesSet.contains("12345"));
-            Assert.assertTrue(zipCodesSet.contains("67890"));
-            Assert.assertEquals(NUMBER_OF_ZIP_CODES.intValue(), zipCodesSet.size());
-        }
+        zipCodeClient.close();
     }
 
     @Test
     @SneakyThrows
     public void testExpandZipCodesWithDuplicationsBetweenAvailableZip() {
-        try (CloseableHttpClient client = HttpClients.createDefault()) {
+        ZipCodeClient zipCodeClient = new ZipCodeClient();
 
-            AuthProvider authProvider = AuthProvider.getInstance();
-            String writeToken = authProvider.getWriteToken();
+        AuthProvider authProvider = AuthProvider.getInstance();
+        String writeToken = authProvider.getWriteToken();
+        String readToken = authProvider.getReadToken();
 
-            String zipCodesExpandUrl = ConfigLoader.getProperty("zipCodesExpandUrl");
+        String zipCodesExpandUrl = ConfigLoader.getProperty("zipCodesExpandUrl");
+        String requestBody = "[\"12345\", \"67890\", \"12345\"]";
 
-            String requestBody = "[\"12345\", \"67890\", \"12345\"]";
+        CloseableHttpResponse response = zipCodeClient.postZipCodes(zipCodesExpandUrl, writeToken, requestBody);
 
-            HttpPost post = new HttpPost(zipCodesExpandUrl);
-            post.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + writeToken);
-            post.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-            post.setEntity(new StringEntity(requestBody, "UTF-8"));
+        int statusCode = response.getStatusLine().getStatusCode();
+        Assert.assertEquals(201, statusCode);
 
-            CloseableHttpResponse response = client.execute(post);
+        String zipCodesUrl = ConfigLoader.getProperty("zipCodesUrl");
+        List<String> zipCodesList = zipCodeClient.getZipCodes(zipCodesUrl, readToken);
+        Set<String> zipCodesSet = new HashSet<>(zipCodesList);
 
-            int statusCode = response.getStatusLine().getStatusCode();
-            Assert.assertEquals(201, statusCode);
+        Assert.assertTrue(zipCodesSet.contains("12345"));
+        Assert.assertTrue(zipCodesSet.contains("67890"));
+        Assert.assertEquals(NUMBER_OF_ZIP_CODES.intValue(), zipCodesSet.size());
 
-            HttpGet get = new HttpGet(ConfigLoader.getProperty("zipCodesUrl"));
-            get.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authProvider.getReadToken());
-            CloseableHttpResponse getResponse = client.execute(get);
-            String getResponseBody = EntityUtils.toString(getResponse.getEntity(), "UTF-8");
-            JsonNode zipCodesNode = new ObjectMapper().readTree(getResponseBody);
-            Set<String> zipCodesSet = new HashSet<>();
-            zipCodesNode.forEach(zipCode -> zipCodesSet.add(zipCode.asText()));
-
-            Assert.assertTrue(zipCodesSet.contains("12345"));
-            Assert.assertTrue(zipCodesSet.contains("67890"));
-
-            Assert.assertEquals(NUMBER_OF_ZIP_CODES.intValue(), zipCodesSet.size());
-        }
+        zipCodeClient.close();
     }
 }
