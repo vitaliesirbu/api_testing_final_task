f5cfa9ea89a9861505da2b7d48005d81d3f85e4a
VitalieSirbu <vitalie.sirbu0@gmail.com>
2024-07-22 11:01:59 +0300

    Adding NoResponseException to git

---
+++ src/main/java/com/coherentsolutions/training/automation/api/sirbu/Utils/NoResponseException.java
@@ -0,0 +1,7 @@
+package com.coherentsolutions.training.automation.api.sirbu.Utils;
+
+public class NoResponseException extends RuntimeException {
+    public NoResponseException(String message){
+        super(message);
+    }
+}
